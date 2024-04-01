f6e1724581263c83a2628a7e3c13a05df6567990
biznezzit <dev@biznezz.consulting>
2024-04-01 14:36:34 +0200

    Customers Page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,9 +1,33 @@
+// import Pagination from '@/app/ui/invoices/pagination';
+// import Search from '@/app/ui/search';
+import Table from '@/app/ui/customers/table';
+// import { lusitana } from '@/app/ui/fonts';
+// import { CustomersTableSkeleton } from '@/app/ui/skeletons';
+import { fetchFilteredCustomers } from '@/app/lib/data';
+// import { Suspense } from 'react';
 import { Metadata } from 'next'; 
 
 export const metadata: Metadata = {
   title: 'Customers'
 }
 
-export default function Page() {
-  return <p>Customers Page</p>;
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+  };
+}) {
+  const query = searchParams?.query || '';
+  // const currentPage = Number(searchParams?.page) || 1;
+  // const totalPages = await fetchCustomersPages(query);
+  // const totalPages = 1;
+  const customers = await fetchFilteredCustomers(query);
+
+  return (
+    <>
+      <Table customers={customers} />
+    </>
+  );
 }
